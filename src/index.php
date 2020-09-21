48be08f3fd0a01a118e5ef95dd932bfc56fd1b21
giuseppe <saraceno-giuseppe@hotmail.com>
2020-09-22 00:33:11 +0200

    handle htt request method

--- src/index.php
+++ src/index.php
@@ -13,14 +13,23 @@ use Saraceno\JsonRpc\Services\Validator;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  header('Method Not Allowed', true, 405);
+  echo "GET method requests are not accepted for this resource";
+  exit;
+}
+
 $requestData = json_decode(file_get_contents('php://input'), true);
 
 
 $validator = new Validator($requestData);
 $validation = $validator->handleRequest();
 
-if (!empty($validation)) 
-    die($validation->asString());
+if (!empty($validation)) {
+    echo $validation->asString();
+    die();
+}
 
 $request = $validator->getRequest();
 $oil_trend = new OilTrend();
@@ -28,6 +37,4 @@ $oil_trend = new OilTrend();
 $server = new Server($oil_trend);
 $response = $server->handleRequest($request);
 
-// die($response->asString());
-print_r($response->asArray());
-// var_dump( $response->asArray());
+die($response->asString());
